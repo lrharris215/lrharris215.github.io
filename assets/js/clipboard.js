45a230f404390d7c4ee62e620394cea85925d3f4
Lane Harris <lrharris215@gmail.com>
2021-04-30 13:27:39 -0400

    created own tooltip to show copy success

---
+++ assets/js/clipboard.js
@@ -0,0 +1,17 @@
+let clipboard = new ClipboardJS('.clipboard');
+clipboard.on('success', function (e) {
+    let email = document.getElementById('email');
+    let tooltip = document.createElement('div');
+    let arrow = document.createElement('div');
+    arrow.className = 'tooltip-arrow';
+    tooltip.className = 'tooltip';
+    tooltip.innerHTML = 'Copied to Clipboard!';
+    tooltip.append(arrow);
+    email.append(tooltip);
+
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
